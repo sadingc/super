ef7fa3addc469f3db85213c9370bbb4311b49161
MarquitoGaming <98572769+MarquitoGaming@users.noreply.github.com>
2022-04-09 16:05:44 -0400

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ class Config(object):
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
-    FREE_USER_MAX_FILE_SIZE = 50000000
+    MAX_FILE_SIZE = 2048
+    TG_MAX_FILE_SIZE = 2048
+    FREE_USER_MAX_FILE_SIZE = 2048
     # chunk size that should be used with requests
-    CHUNK_SIZE = int(123404684308)
+    CHUNK_SIZE = int(128)
     # default thumbnail to be used in the videos
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
