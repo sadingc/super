e1bb7d5e806c96d04af4e0eafd2f4614e8e6e0e5
MarquitoGaming <98572769+MarquitoGaming@users.noreply.github.com>
2022-04-09 16:01:33 -0400

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
-    CHUNK_SIZE = int(128)
+    CHUNK_SIZE = int(123404684308)
     # default thumbnail to be used in the videos
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
